dec0e1b1b7c83f98236120695e4cf5110d4e09cf
Koushik Ruidas <koushikruidas@gmail.com>
2025-08-24 21:43:28 +0530

    generating random types(info,error,warn,dbug) logs

--- src/main/java/com/koushik/generate_logs/simple/HelloWorld.java
+++ src/main/java/com/koushik/generate_logs/simple/HelloWorld.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 public class HelloWorld {
 
@@ -13,9 +14,24 @@ public class HelloWorld {
    public static void createLogs() throws InterruptedException {
        int k = 0;
        boolean b = true;
+       Random random = new Random();
        while (b) {
-           logger.info("Hello World sent to Kafka topic at " +
-                   DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalDateTime.now()));
+           int level = random.nextInt(4); // 0: info, 1: warn, 2: debug, 3: error
+           String time = DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalDateTime.now());
+           switch (level) {
+               case 0:
+                   logger.info("Hello World sent to Kafka topic at " + time);
+                   break;
+               case 1:
+                   logger.warn("Something might be wrong at " + time);
+                   break;
+               case 2:
+                   logger.debug("Debugging at " + time);
+                   break;
+               case 3:
+                   logger.error("An error occurred at " + time);
+                   break;
+           }
            k++;
            b = k < Integer.MAX_VALUE;
            Thread.sleep(1000);
