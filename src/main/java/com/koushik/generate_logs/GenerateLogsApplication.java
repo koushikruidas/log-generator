d13f061feb7e924b47cb219dd3ef379415b4350d
Koushik <koushikruidas@gmail.com>
2025-11-17 17:49:44 +0530

    Using latest jar and Removed Component scanner

--- src/main/java/com/koushik/generate_logs/GenerateLogsApplication.java
+++ src/main/java/com/koushik/generate_logs/GenerateLogsApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.poinciana.loganalyzerClient"})
 public class GenerateLogsApplication {
 
 	public static void main(String[] args) throws InterruptedException {
